963baeb5d5139922ac4eb02dbadd1c7d29f68610
Raz <raz@yosigal.com>
2018-03-29 18:28:20 +0300

    Fixed main and makefile

--- ex1/main.cpp
+++ ex1/main.cpp
@@ -1,33 +1,30 @@
 //
 // Created by heimy4prez on 3/29/18.
 //
-#include osm.h
-
+#include <iostream>
+#include "osm.h"
 
 int main(int argc, char** argv){
-    if (argc != 1){
-        std::cerr << "Must provide the number of iterations as an arguments.";
+    if (argc != 2){
+        std::cerr << "Must provide the number of iterations as an arguments." << std::endl;
         return -1;
     }
-    unsigned int iterations = std::stoi(argv[0]);
+    unsigned int iterations = std::stoi(argv[1]);
     double t;
 
-    std::cout << "Running 3 tests on " << iterations << "iterations.";
+    std::cout << "Running 3 tests on " << iterations << " iterations." << std::endl;
 
-    std::cout << "Running arithmetic test...";
+    std::cout << "Running arithmetic test..." << std::endl;
     t = osm_operation_time(iterations);
-    std::cout << "Full interval: " << t << ". Per iteration: " << (t / iterations);
+    std::cout << "Full interval: " << t << ". Per iteration: " << (t / iterations) << std::endl;
 
-    std::cout << "Running function call test...";
+    std::cout << "Running function call test..." << std::endl;
     t = osm_function_time(iterations);
-    std::cout << "Full interval: " << t << ". Per iteration: " << (t / iterations);
+    std::cout << "Full interval: " << t << ". Per iteration: " << (t / iterations) << std::endl;
 
-    std::cout << "Running sys trap test...";
+    std::cout << "Running sys trap test..." << std::endl;
     t = osm_syscall_time(iterations);
-    std::cout << "Full interval: " << t << ". Per iteration: " << (t / iterations);
-
-
+    std::cout << "Full interval: " << t << ". Per iteration: " << (t / iterations) << std::endl;
 
     return 0;
-
 }
